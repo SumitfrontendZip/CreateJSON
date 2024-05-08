a99eaed4c4730ffe93fcd68580c49254f893da0f
Sumit Kumar <sumit@frontendzip.com>
2024-05-08 12:48:09 +0530

    write Html in index.html and create a from in input tags

---
+++ index.js
@@ -0,0 +1,7 @@
+// Importing the object dsaData from ds.js
+import { dsaData } from "./ds";
+
+// Iterate over the values of dsaData and log each value
+Object.values(dsaData).forEach((value) => {
+    console.log(value);
+});
