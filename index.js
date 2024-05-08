8e00682928216e71c134829d2a7d1d3ed5b0fbd0
Sumit Kumar <sumit@frontendzip.com>
2024-05-08 13:45:49 +0530

    create fs module and use fs module to create file

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
-const dsaDATA = {}
+const dsaDATA = {};
 
-class json{
-    constructor(slug,subtitle,diff, time, rec, cURL, SURL, QD){
-        this.slug = slug;
-        this.title = slug;
-        this.subtitle = subtitle;
-        this.difficulty = diff;
-        this.comapany = "Leetcode";
-        this.QuestionType = "DSA";
-        this.technology = "JavaScript";
-        this.time = time;
-        this.recommended = rec;
-        this.codingURL = cURL;
-        this.solutionURL = SURL;
-        this.QuestionDescription = QD;
-    }
+class json {
+  constructor(slug, subtitle, diff, time, rec, cURL, SURL, QD) {
+    this.slug = slug;
+    this.title = slug;
+    this.subtitle = subtitle;
+    this.difficulty = diff;
+    this.comapany = "Leetcode";
+    this.QuestionType = "DSA";
+    this.technology = "JavaScript";
+    this.time = time;
+    this.recommended = rec;
+    this.codingURL = cURL;
+    this.solutionURL = SURL;
+    this.QuestionDescription = QD;
+  }
 }
+const submit = document.getElementById("submit");
 
-const slug = document.getElementById("title");
-const subtitle = document.getElementById("Subtitle")
-const difficulty = document.getElementById("difficulty")
-const time = document.getElementById("time")
-const recommended = document.getElementById("recommended")
-const codingURL = document.getElementById("codingURL")
-const solutionURL = document.getElementById("SolutionURL")
-const QuestionDescription = document.getElementById("QuestionDec")
+submit.addEventListener("click", () => {
+  const slug = document.getElementById("title").value;
+  const subtitle = document.getElementById("Subtitle").value;
+  const difficulty = document.getElementById("difficulty").value;
+  const time = document.getElementById("time").value;
+  const recommended = document.getElementById("recommended").value;
+  const codingURL = document.getElementById("codingURL").value;
+  const solutionURL = document.getElementById("SolutionURL").value;
+  const QuestionDescription = document.getElementById("QuestionDec").value;
 
+  const data = new json(
+    slug,
+    subtitle,
+    difficulty,
+    time,
+    recommended,
+    codingURL,
+    solutionURL,
+    QuestionDescription
+  );
 
+  const fs = require('fs')
+});
