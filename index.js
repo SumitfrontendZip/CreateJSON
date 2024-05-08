c7c97fd2055ee24f726b068fae88ae5bc748390d
Sumit Kumar <sumit@frontendzip.com>
2024-05-08 13:12:15 +0530

    write js and get all values in input box

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-// Importing the object dsaData from ds.js
-import { dsaData } from "./ds";
+const dsaDATA = {}
 
-// Iterate over the values of dsaData and log each value
-Object.values(dsaData).forEach((value) => {
-    console.log(value);
-});
+class json{
+    constructor(slug,subtitle,diff, time, rec, cURL, SURL, QD){
+        this.slug = slug;
+        this.title = slug;
+        this.subtitle = subtitle;
+        this.difficulty = diff;
+        this.comapany = "Leetcode";
+        this.QuestionType = "DSA";
+        this.technology = "JavaScript";
+        this.time = time;
+        this.recommended = rec;
+        this.codingURL = cURL;
+        this.solutionURL = SURL;
+        this.QuestionDescription = QD;
+    }
+}
+
+const slug = document.getElementById("title");
+const subtitle = document.getElementById("Subtitle")
+const difficulty = document.getElementById("difficulty")
+const time = document.getElementById("time")
+const recommended = document.getElementById("recommended")
+const codingURL = document.getElementById("codingURL")
+const solutionURL = document.getElementById("SolutionURL")
+const QuestionDescription = document.getElementById("QuestionDec")
+
+
